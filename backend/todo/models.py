d98ad52b63c8dbf426a702299ae7546514d16b4b
Modou Diao <modou.diao@wizall.com>
2021-03-26 13:04:28 +0000

    add front app

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -7,5 +7,5 @@ class Todo(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
 
-    def _str_(self)00:
+    def _str_(self):
         return self.title
